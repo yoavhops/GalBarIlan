05f6bc0c057950a789d6a6bb26046cdd2284b1fe
yoavhops <yoav.hops@gmail.com>
2019-07-01 11:13:07 +0300

    First Commit

---
+++ Assets/Scripts/SoundStarter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SoundStarter : MonoBehaviour
+{
+    public AudioSource Debug;
+    public float Delay = 0;
+
+    void OnEnable()
+    {
+        if (Debug != null)
+        {
+            Debug.time = Delay;
+            Debug.Play();
+        }
+    }
+
+}
